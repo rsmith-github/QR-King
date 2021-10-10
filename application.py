58bc61c9f32f73105d861439bbf974ba2d06eb53
rsmith-github <rsmith.git@gmail.com>
2021-10-10 15:22:46 +0100

    change to postgres database

--- application.py
+++ application.py
@@ -33,7 +33,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///project.db")
+db = SQL(os.getenv("DATABASE_URL"))
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
