9bd8cdfbaa5c914c34b047e932d994c28734d931
rsmith-github <rsmith.git@gmail.com>
2021-10-10 16:37:34 +0100

    sesion permanent = false

--- application.py
+++ application.py
@@ -28,11 +28,11 @@ def after_request(response):
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+# Configure CS50 Library to use SQL database
 db = SQL(os.getenv("DATABASE_URL"))
 
 # Make sure API key is set
