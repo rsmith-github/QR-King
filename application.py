09f791f2f6ed35597fccf30e4acb66f77c94ad0b
rsmith-github <rsmith.git@gmail.com>
2021-10-10 11:40:33 +0100

    attempt to update requirements

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 import qrcode
 
 from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
