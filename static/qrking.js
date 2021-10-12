5c43f5e080389bcb573c270b67f26af1c40e78f9
rsmith-github <rsmith.git@gmail.com>
2021-10-12 18:24:42 +0100

    add javascript

---
+++ static/qrking.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let linkForm = document.querySelector(".link");
+    linkForm.onsubmit = function() {
+        let alert = document.querySelector('.alert')
+        if (alert.innerHTML.includes("URL")) {
+            alert.style.backgroundColor = "#ED4337";
+            alert.style.color = "white";
+            alert.style.border = "none";
+            alert.classList.remove(".border");
+        }
+    }
+});
