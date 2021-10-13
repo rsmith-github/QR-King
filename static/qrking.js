2d515dc37c45282f9d981ca8892ba2af1730f10e
rsmith-github <rsmith.git@gmail.com>
2021-10-13 22:56:28 +0100

    add color flask.flash messages

--- static/qrking.js
+++ static/qrking.js
@@ -1,15 +1,1 @@
-// Under construction
-// document.addEventListener("DOMContentLoaded", function () {
-
-//     let linkForm = document.querySelector("#link");
-//     linkForm.onsubmit = function() {
-//         let alert = document.querySelector('.alert')
-//         if (alert.innerHTML.includes("URL")) {
-//             alert.style.backgroundColor = "#ED4337";
-//             alert.style.color = "white";
-//             alert.style.border = "none";
-//             alert.classList.remove(".border");
-//         }
-//         return false;
-//     }
-// });
+// JS File
