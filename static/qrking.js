7b803e8314df3e477e4be2cc464efdc005e55d8d
rsmith-github <rsmith.git@gmail.com>
2021-10-17 02:13:36 +0100

    improve javascript

--- static/qrking.js
+++ static/qrking.js
@@ -7,9 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     //     alert.classList.remove("border");
     // }
 
+    
     var loc = window.location.href; // returns the full URL
-    if(/login/.test(loc) || /register/.test(loc) || loc === "https://qrking.herokuapp.com") {
+    if(/login/.test(loc) || /register/.test(loc)) {
         document.querySelector("main").style.marginTop = "10%";
       }
+
+    //   add margin to index page
+    document.querySelector(".index-headline").parentElement.style.marginTop = "15%";
 
 });
