ba1319fce84ee4646a00248b856e325a65696de7
rsmith-github <rsmith.git@gmail.com>
2021-10-17 01:49:29 +0100

    marign etc

--- static/qrking.js
+++ static/qrking.js
@@ -1,10 +1,15 @@
 // Under construction
 document.addEventListener("DOMContentLoaded", function () {
 
-    let alert = document.querySelector('.alert')
-    if (alert.innerHTML.includes("URL")) {
-        alert.classList.add("error");
-        alert.classList.remove("border");
-    }
+    // let alert = document.querySelector('.alert')
+    // if (alert.innerHTML.includes("URL")) {
+    //     alert.classList.add("error");
+    //     alert.classList.remove("border");
+    // }
+
+    var loc = window.location.href; // returns the full URL
+    if(/login/.test(loc)) {
+        document.querySelector("main").style("marginTop", "10%")
+      }
 
 });
