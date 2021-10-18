f1dc1c99a4aed2fdeef80284ddfa1ba58e077e8c
rsmith-github <rsmith.git@gmail.com>
2021-10-18 02:15:22 +0100

    add hidden form

--- static/qrking.js
+++ static/qrking.js
@@ -1,17 +1,17 @@
 // Under construction
 document.addEventListener("DOMContentLoaded", function () {
 
-    // let alert = document.querySelector('.alert')
-    // if (alert.innerHTML.includes("URL")) {
-    //     alert.classList.add("error");
-    //     alert.classList.remove("border");
-    // }
+    let alert = document.querySelector('.alert')
+    if (alert.innerHTML.includes("URL")) {
+        alert.classList.add("error");
+        alert.classList.remove("border");
+    }
 
     
     var loc = window.location.href; // returns the full URL
     if(/login/.test(loc) || /register/.test(loc)) {
         document.querySelector("main").style.marginTop = "10%";
-      }
+    }
 
     //   add margin to index page
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
