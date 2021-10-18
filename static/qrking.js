5284f9270d2c4a5c0c54717e8af8e494a97f5b15
rsmith-github <rsmith.git@gmail.com>
2021-10-18 03:25:45 +0100

    js hidden footer

--- static/qrking.js
+++ static/qrking.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
   if(/login/.test(loc) || /register/.test(loc)) {
     document.querySelector("main").style.marginTop = "10%";
   }
+
+  if (document.querySelector(".hidden_footer")) {
+    alert("Please provide valid URL");
+  }
   
   //   add margin to index page
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
